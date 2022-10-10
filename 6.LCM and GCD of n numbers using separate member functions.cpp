da6fdbba92d4afaef571576d996c09e148594386
Mr.Jayanth <112700471+Jayanth0124@users.noreply.github.com>
2022-10-11 00:21:26 +0530

    Update 6.LCM and GCD of n numbers using separate member functions.cpp

--- 6.LCM and GCD of n numbers using separate member functions.cpp
+++ 6.LCM and GCD of n numbers using separate member functions.cpp
@@ -1,44 +1,51 @@
 #include<iostream>
 using namespace std;
-
-class LCMGCD {
-	private:
-		int a, b, x, y, t, gcd, lcm;
-	public:
-		void input() {
-			cout<<"Enter two integers: ";
-			cin>>x>>y;
+int lcm(int a,int b)
+{
+	int max=a>b?a:b;
+	while(1)
+	{
+		if(max%a==0 && max%b==0)
+		{
+			return max;
+			break;
 		}
-		void compute_lcm() {
-			a=x;
-			b=y;
-			while(b!=0) {
-				t=b;
-				b=a%b;
-				a=t;
-			}
-			gcd=a;
-			lcm=(x*y)/gcd;
-			cout<<"\nLCM = "<<lcm;
+		++max;
+	}
+}
+int gcd(int a,int b)
+{
+	int min=a<b?a:b;
+	while(1)
+	{
+		if(a%min==0 && b%min==0)
+		{
+			return min;
+			break;
 		}
-
-		void compute_gcd() {
-			a=x;
-			b=y;
-			while(b!=0) {
-				t=b;
-				b=a%b;
-				a=t;
-			}
-			gcd=a;
-			cout<<"GCD = "<<gcd;
-		}
-};
-
-int main() {
-	LCMGCD obj;
-	obj.input();
-	obj.compute_lcm();
-	obj.compute_gcd();
+		--min;
+	}
+}
+int main()
+{
+	int n,a,b;
+	int array[10];
+	cout<<"N value = ";
+	cin>>n;
+	cout<<"Number 1 = ";
+	cin>>a;
+	cout<<"Number 2 = ";
+	cin>>b;
+	int l=lcm(a,b);
+	int g=gcd(a,b);
+	for(int i=3;i<=n;i++)
+	{
+		cout<<"Number "<<i<<" = ";
+		cin>>array[i];
+		l=lcm(l,array[i]);
+		g=gcd(g,array[i]);
+	}
+	cout<<"LCM = "<<l<<"\n";
+	cout<<"GCD = "<<g<<"\n";
 	return 0;
 }
