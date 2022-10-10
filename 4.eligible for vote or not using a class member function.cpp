78d246e131976125c2bb380033af70ab3ef659d8
Mr.Jayanth <112700471+Jayanth0124@users.noreply.github.com>
2022-10-11 00:04:04 +0530

    Update 4.eligible for vote or not using a class member function.cpp

--- 4.eligible for vote or not using a class member function.cpp
+++ 4.eligible for vote or not using a class member function.cpp
@@ -1,23 +1,80 @@
 #include<iostream>
+#include<string>
 using namespace std;
-class vote {
-		int age;
-	public:
-		vote() {
-			age=0;
-		}
-		int check(int a) {
-			if(a>=18) {
-				cout<<"You are allowed to vote";
-			} else {
-				cout<<"You are allowed to vote when you will be of "<<18-a<<" years";
-			}
-		}
+
+class Person
+{
+    private:
+        string ssn;
+        int age;
+    
+    public:
+        Person()
+        {
+            ssn = "000000000";
+            age = 0;
+        }
+        void set_ssn(string ssn)
+        {
+            if(ssn.length()!= 9)
+            {
+                cout<<"Invalid SSN"<<endl;
+                return;
+            }
+            for(int i = 0; i < ssn.length(); i++)
+            {
+                if(!isdigit(ssn[i]))
+                {
+                    cout<<"Invalid SSN"<<endl;
+                    return;
+                }
+            }
+            this -> ssn = ssn;
+        }
+        string get_ssn()
+        {
+            return ssn;
+        }
+        void set_age(int age)
+        {
+            if(age < 18)
+            {
+                cout<<"Invalid Input"<<endl;
+                return;
+            }
+            this -> age = age;
+        }
+        int get_age()
+        {
+            return age;
+        }
+        bool check_eligibility()
+        {
+            if(age >= 18)
+            {
+                return true;
+            }
+            return false;
+        }
 };
-int main() {
-	vote v;
-	int age;
-	cout<<"Enter your age";
-	cin>>age;
-	v.check(age);
+
+int main()
+{
+    Person obj;
+    string ssn;
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
+    obj.set_age(age);
+    
+    if(obj.check_eligibility())
+    {
+        cout<<"Eligible"<<endl;
+    }
+    else
+    {
+        cout<<"Not eligible"<<endl;
+    }
+    
+    return 0;
 }
